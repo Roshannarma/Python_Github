0129f1fe6c8b0be851084055d5df315b3b4c9fb4
Roshan Narma <rnarma@stevens.edu>
2020-07-03 11:11:11 -0400

    cleaned up extra code and commented through john hopkins api

--- John_hopkins_api/Human_side_interface.py
+++
@@ -1,5 +0,0 @@
-import basic_spell_check as bsc
-f = open("D:\Python_Github\John_hopkins_api\some_countries.txt","r")
-my_dict = eval(f.readline())
-my_input = input("Give a Country you want to look up: ")
-print(bsc.spell_check(my_dict,my_input))
--- John_hopkins_api/testing.py
+++ John_hopkins_api/testing.py
@@ -3,28 +3,33 @@ from matplotlib import pyplot as plt
 import numpy as np
 import datetime as dt
 import matplotlib.dates as mdates
+# normally called SpellChecker but had to move into file to edit code for my use
 import better_spell_check as bsc
 import time
+from tkinter import *
+
+# Extracting data from txt files
 f = open("D:\Python_Github\John_hopkins_api\some_countries.txt","r")
 my_dict = eval(f.readline())
 f.close()
-my_input = input("Give a Country you want to look up: ")
 f = open("D:\Python_Github\John_hopkins_api\slugs.txt","r")
 my_slugs_list = eval(f.readline())
 f.close()
-#mytime = time.time()
+
+#taking in input from user
+my_input = input("Give a Country you want to look up: ")
 my_country = bsc.spell_checker(my_input)
-#print(time.time()-mytime)
 print(my_country)
 my_slug = my_slugs_list[my_dict.index(my_country)]
-#print(my_slug)
-url = "https://api.covid19api.com/total/dayone/country/" +  my_slug + "/status/confirmed"
 
+# gathering data from api based on country
+url = "https://api.covid19api.com/total/dayone/country/" +  my_slug + "/status/confirmed"
 payload = {}
 headers= {}
-
 response = requests.request("GET", url, headers=headers, data = payload)
 response_json = response.json()
+
+#organizing my data
 my_data = []
 my_sum = 0
 my_dates = []
@@ -33,11 +38,12 @@ for Date in response_json:
     my_data.append(Date["Cases"]-my_sum)
     my_sum += (Date["Cases"] - my_sum)
 
-#    '2020-03-15T00:00:00Z'
+# changing the dates to a readable and useable format
 x = []
 for d in my_dates:
     x.append(dt.datetime.strptime(d[0:10],'%Y-%m-%d').date())
-#print(x)
+
+#creating a seven-day-sum
 seven_day_sum = []
 length = len(my_data)
 for location in range(0,length):
@@ -50,10 +56,10 @@ for location in range(0,length):
     else:
         seven_day_sum_temp = sum(my_data[location-3:location+4])
         seven_day_sum.append(seven_day_sum_temp/7)
+
+# Setting up graph properties and displaying
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = 14))
-#index_line = np.arange(len(seven_day_sum))
-#index = np.arange(len(my_data))
 seven_day_sum_graph = plt.plot(x,seven_day_sum, color = "red", Label = "7 day sum")
 my_display = plt.bar(x,my_data,1, Label = "Daily Charts")
 plt.tight_layout()
